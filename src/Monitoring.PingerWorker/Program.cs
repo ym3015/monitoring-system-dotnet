4dfacd19846ad373b10d56fb0033446731135b6e
Majid Yavari <yavari.majid@gmail.com>
2026-04-30 18:56:31 +0330

    Initial project setup

---
+++ src/Monitoring.PingerWorker/Program.cs
@@ -0,0 +1,13 @@
+namespace Monitoring.PingerWorker;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
